2fbbdf6ae4d80b90aa582e30bba50b21b06535dc
mishkasakhelashvili <56650453+mishkasakhelashvili@users.noreply.github.com>
2020-06-01 18:20:12 +0400

    add

---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit molestiae a distinctio dignissimos at laboriosam itaque minus, nesciunt et officiis nulla impedit perferendis commodi ipsum excepturi id odit porro fuga.</p>
+  </div>
+</template>
